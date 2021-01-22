862bc67251143a6e51f3f8598a3cc9fd05cf19ea
Jack Witt <jwitt1452@gmail.com>
2021-01-22 15:48:40 -0800

    Added comments to ItemModel.cs

--- Mine/Mine/Models/ItemModel.cs
+++ Mine/Mine/Models/ItemModel.cs
@@ -2,13 +2,21 @@
 
 namespace Mine.Models
 {
+    /*
+     * Items for the Characters and Monsters to use. 
+     */
     public class ItemModel
     {
+        // The id for the Item
         public string Id { get; set; }
+
+        // The Display Text for the Item
         public string Text { get; set; }
+
+        // The Description for the Item
         public string Description { get; set; }
 
-        // The value of an item +9 damage
+        // The value of an Item +9 damage
         public int Value { get; set; } = 0;
     }
 }
