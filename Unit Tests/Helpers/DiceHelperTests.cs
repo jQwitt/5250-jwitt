2c971bfe2e379c006f4460a69d2be2f19c4c0010
Jack Witt <jwitt1452@gmail.com>
2021-02-07 15:09:56 -0800

    Added RollDice_Invalid_Roll_Forced_1_Expect_1 test to DiceHelperTests.cs

--- Unit Tests/Helpers/DiceHelperTests.cs
+++ Unit Tests/Helpers/DiceHelperTests.cs
@@ -38,5 +38,22 @@ namespace Unit_Tests.Helpers
             Assert.AreEqual(true, result >= 1);
             Assert.AreEqual(true, result <= 6);
         }
+
+        [Test]
+        public void RollDice_Invalid_Roll_Forced_1_Expect_1()
+        {
+            // Arrange
+            DiceHelper.ForeRollsToNotRandom = true;
+            DiceHelper.ForcedRandomValue = 1;
+
+            // Act
+            var result = DiceHelper.RollDice(1, 1);
+
+            // Reset
+            DiceHelper.ForeRollsToNotRandom = false;
+
+            // Assert
+            Assert.AreEqual(1, result);
+        }
     }
 }
